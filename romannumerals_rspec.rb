664ec679870807d27fe8c68cd3866a96ef478b2f
RayHightower <rth@wisdomgroup.com>
2012-03-03 08:57:00 -0600

    file name change: romannumerals_rspec.rb

--- romannumerals_rspec.rb
+++ romannumerals_rspec.rb
@@ -1,11 +1,11 @@
 #romannumerals_spec
-require 'romannumerals'
+require_relative 'romannumerals'
 
 describe "Roman Numerals" do
 
   context "Convert I" do
     it "should convert number 1 to Roman Numeral I" do
-      @roman.convert(1).should == "I"
+      RomanNumerals.convert(1).should == "I"
     end
   end
 end
