11b371adc362ba1ea4ecd3e7586ea9d21ee8e070
RayHightower <rth@wisdomgroup.com>
2012-03-02 22:54:13 -0600

    added first test

--- romannumerals_spec.rb
+++ romannumerals_spec.rb
@@ -1,3 +1,11 @@
 #romannumerals_spec
 require 'romannumerals'
 
+describe "Roman Numerals" do
+
+  context "Convert I" do
+    it "should convert number 1 to Roman Numeral I" do
+      @roman.convert(1).should == "I"
+    end
+  end
+end
