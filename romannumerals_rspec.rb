482a479f66a1e59c6d7d79f01fa7f9f8928ac9cc
RayHightower <rth@wisdomgroup.com>
2012-03-03 22:11:00 -0600

    small steps

--- romannumerals_rspec.rb
+++ romannumerals_rspec.rb
@@ -4,8 +4,13 @@ require_relative 'romannumerals'
 describe "Roman Numerals" do
 
   context "Convert I" do
-    it "should convert number 1 to Roman Numeral I" do
+    it "should convert arabic number 1 to Roman Numeral I" do
       RomanNumerals.convert(1).should == "I"
+    end
+  end
+  context "Convert II" do
+    it "should convert arabic number 2 to Roman Numeral II" do
+      RomanNumerals.convert(2).should == "II"
     end
   end
 end
