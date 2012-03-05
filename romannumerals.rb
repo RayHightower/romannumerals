f3aad7703ba34cc3112cc277b04c6d688593c006
RayHightower <rth@wisdomgroup.com>
2012-03-04 19:28:22 -0600

    added .gitignore

--- romannumerals.rb
+++ romannumerals.rb
@@ -1,11 +1,17 @@
 class RomanNumerals
+
   def self.convert(number)
+    
     if number == 1
       result = "I"
     end
+
     if number == 2
       result = "II"
     end
+
     return result
+
   end
+
 end
