f489488bcc8f70880067fe355a9cdb912111c782
RayHightower <rth@wisdomgroup.com>
2012-04-08 21:52:43 -0500

    before working

--- lib/romannumeral.rb
+++ lib/romannumeral.rb
@@ -3,7 +3,7 @@
 #
 class RomanNumeral
 
-  NUMERALS = { 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
+  ROMANNUMERALS = { 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
       
   def self.convert(arabic)
     
