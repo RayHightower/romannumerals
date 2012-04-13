2be0975297cbf605371674f0c407389505109f7a
RayHightower <rth@wisdomgroup.com>
2012-04-13 09:41:13 -0500

    so cool when the tests test the code

--- kata01/lib/arabic2roman.rb
+++ kata01/lib/arabic2roman.rb
@@ -2,12 +2,38 @@ class Arabic2Roman
 
   def self.convert (arabic)
 
-    return "I" if arabic == 1
-    return "II" if arabic == 2
-    return "III" if arabic == 3
-    
+    roman = ""
+
     return "V" if arabic == 5
     return "X" if arabic == 10
+    return "L" if arabic == 50
+
+    while arabic >= 10
+      roman += "X"
+      arabic -= 10
+    end
+
+    if arabic == 9 
+      roman += "IX"
+      arabic -= 9
+    end
+
+    if arabic >= 5 
+      roman += "V"
+      arabic -= 5
+    end
+
+    if arabic == 4 
+      roman += "IV"
+      arabic -= 4
+    end
+
+    while arabic >= 1
+      roman += "I"
+      arabic -= 1
+    end
+
+    return roman
 
   end
 
--- kata01/spec/arabic2roman_spec.rb
+++ kata01/spec/arabic2roman_spec.rb
@@ -1,6 +1,14 @@
 require_relative '../lib/arabic2roman.rb'
 
+ROMANLIST = { 1 => "I", 2 => "II", 4 => "IV", 5 => "V", 6 => "VI", 9 => "IX", 10 => "X", 14 => "XIV", 19 => "XIX", 20 => "XX" }
+
 describe "Arabic to Roman" do
+
+  ROMANLIST.each do |arabic, roman|
+    it "should convert Arabic to Roman from the list" do
+      Arabic2Roman.convert(arabic).should == roman
+    end
+  end
 
   context "Convert Arabic 1, 2, & 3 to Roman" do
     it "should convert Arabic 1 to Roman I" do
@@ -13,14 +21,29 @@ describe "Arabic to Roman" do
       Arabic2Roman.convert(3).should == "III"
     end
   end
+  context "Convert Arabic 4 to IV" do
+    it "should convert Arabic 4 to Roman IV" do
+      Arabic2Roman.convert(4).should == "IV"
+    end
+  end
   context "Convert Arabic 5 to V" do
     it "should convert Arabic 5 to Roman V" do
       Arabic2Roman.convert(5).should == "V"
     end
   end
+  context "Convert Arabic 6 to IV" do
+    it "should convert Arabic 6 to Roman IV" do
+      Arabic2Roman.convert(6).should == "VI"
+    end
+  end
   context "Convert Arabic 10 to X" do
     it "should convert Arabic 10 to Roman X" do
       Arabic2Roman.convert(10).should == "X"
+    end
+  end
+  context "Convert Arabic 50 to L" do
+    it "should convert Arabic 50 to Roman L" do
+      Arabic2Roman.convert(50).should == "L"
     end
   end
 end
