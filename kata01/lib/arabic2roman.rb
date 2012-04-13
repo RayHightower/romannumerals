1379f611cf1dfdcb47e76a048e869ef153bf0016
RayHightower <rth@wisdomgroup.com>
2012-04-12 23:13:58 -0500

    I thru III are covered

--- kata01/lib/arabic2roman.rb
+++ kata01/lib/arabic2roman.rb
@@ -2,7 +2,9 @@ class Arabic2Roman
 
   def self.convert (arabic)
 
-    return "I"
+    return "I" if arabic == 1
+    return "II" if arabic == 2
+    return "III" if arabic == 3
 
   end
 
--- kata01/spec/arabic2roman_spec.rb
+++ kata01/spec/arabic2roman_spec.rb
@@ -2,9 +2,15 @@ require_relative '../lib/arabic2roman.rb'
 
 describe "Arabic to Roman" do
 
-  context "Convert Arabic 1 to Roman I" do
+  context "Convert Arabic 1, 2, & 3 to Roman" do
     it "should convert Arabic 1 to Roman I" do
       Arabic2Roman.convert(1).should == "I"
+    end
+    it "should convert Arabic 2 to Roman II" do
+      Arabic2Roman.convert(2).should == "II"
+    end
+    it "should convert Arabic 3 to Roman III" do
+      Arabic2Roman.convert(3).should == "III"
     end
   end
 end
