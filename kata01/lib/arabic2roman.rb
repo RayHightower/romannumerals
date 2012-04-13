5f22fde02bc19c1b6c77f0be8531f93eef080f22
RayHightower <rth@wisdomgroup.com>
2012-04-13 11:26:00 -0500

    better iterative algorithm for execution. gotta do this with other languages.

--- kata01/lib/arabic2roman.rb
+++ kata01/lib/arabic2roman.rb
@@ -1,5 +1,10 @@
 class Arabic2Roman
 
+  ROMANBRICKS = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD",
+                  100 => "C", 90 => "XC", 50 => "L", 40 => "XL",
+                  10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 
+                  1 => "I" }
+
   def self.convert (arabic)
 
     roman = ""
@@ -7,64 +12,11 @@ class Arabic2Roman
     return "Overflow." if arabic > 3000
     return "Nothing less than one." if arabic < 1
 
-    while arabic >= 1000
-      roman += "M"
-      arabic -= 1000
-    end
-
-    if arabic >= 900
-      roman += "CM"
-      arabic -= 900
-    end
-
-    while arabic >= 500
-      roman += "D"
-      arabic -= 500
-    end
-    
-    while arabic >= 100
-      roman += "C"
-      arabic -= 100
-    end
-    
-    if arabic >= 90
-      roman += "XC"
-      arabic -= 90
-    end
-    
-    if arabic >= 50
-      roman += "L"
-      arabic -= 50
-    end
-    
-    if arabic >= 40
-      roman += "XL"
-      arabic -= 40
-    end
-
-    while arabic >= 10
-      roman += "X"
-      arabic -= 10
-    end
-
-    if arabic == 9 
-      roman += "IX"
-      arabic -= 9
-    end
-
-    if arabic >= 5 
-      roman += "V"
-      arabic -= 5
-    end
-
-    if arabic == 4 
-      roman += "IV"
-      arabic -= 4
-    end
-
-    while arabic >= 1
-      roman += "I"
-      arabic -= 1
+    ROMANBRICKS.each do |arabicbrick, romanbrick|
+      while arabic >= arabicbrick
+        roman += romanbrick
+        arabic -= arabicbrick
+      end
     end
 
     return roman
