83fe4a248594a35b093ddf703356a5e283a4d016
RayHightower <rth@wisdomgroup.com>
2012-04-15 17:03:35 -0500

    now up to Arabic 10

--- kata02/lib/arabic2roman.rb
+++ kata02/lib/arabic2roman.rb
@@ -1,7 +1,30 @@
 class Arabic2Roman
 
   def self.convert(arabic)
-    return "I"
+    roman = ""
+
+    if arabic == 10
+      roman += "X"
+      arabic -= 10
+    end
+
+    if arabic == 5
+      roman += "V"
+      arabic -= 5
+    end
+
+    if arabic == 4
+      roman += "IV"
+      arabic -= 4
+    end
+
+    while arabic >= 1
+      roman += "I"
+      arabic -= 1
+    end
+
+    return roman
+    
   end
 
 end
--- kata02/spec/arabic2roman_spec.rb
+++ kata02/spec/arabic2roman_spec.rb
@@ -5,5 +5,21 @@ describe "Start with 1" do
     Arabic2Roman.convert(1).should == "I"
   end
   
+  it "should convert Arabic 2 to Roman II" do
+    Arabic2Roman.convert(2).should == "II"
+  end
+
+  it "should convert Arabic 4 to Roman IV" do
+    Arabic2Roman.convert(4).should == "IV"
+  end
+
+  it "should convert Arabic 5 to Roman V" do
+    Arabic2Roman.convert(5).should == "V"
+  end
+
+  it "should convert Arabic 10 to Roman X" do
+    Arabic2Roman.convert(10).should == "X"
+  end
+
 end
 
