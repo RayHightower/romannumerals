08d1e3c41f7b47eb2e25953b7837672f7d440ad4
RayHightower <rth@wisdomgroup.com>
2012-04-15 17:41:54 -0500

    kata complete

--- kata02/lib/arabic2roman.rb
+++ kata02/lib/arabic2roman.rb
@@ -1,10 +1,19 @@
 class Arabic2Roman
 
 
-A2RTABLE = { 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
+A2RTABLE = { 1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 
+             90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
   
   def self.convert(arabic)
     roman = ""
+
+    if arabic > 3000
+      return "Overflow. Nothing larger than 3000."
+    end
+
+    if arabic < 1
+      return "Underflow. Nothing less than zero."
+    end
 
     A2RTABLE.each do |avalue, rvalue|
       while arabic >= avalue do
@@ -15,34 +24,6 @@ A2RTABLE = { 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
 
     return roman
 
-
-    while arabic >= 10
-      roman += "X"
-      arabic -= 10
-    end
-
-    if arabic == 9
-      roman += "IX"
-      arabic -= 9
-    end
-
-    if arabic == 5
-      roman += "V"
-      arabic -= 5
-    end
-
-    if arabic == 4
-      roman += "IV"
-      arabic -= 4
-    end
-
-    while arabic >= 1
-      roman += "I"
-      arabic -= 1
-    end
-
-    return roman
-    
   end
 
 end
--- kata02/spec/arabic2roman_spec.rb
+++ kata02/spec/arabic2roman_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../lib/arabic2roman'
 
 ROMANTABLE = { 1 => "I", 2 => "II", 4 => "IV", 5 => "V", 9 => "IX",
-               10 => "X", 14 => "XIV", 19 => "XIX", 24 => "XXIV", 40 => "XL" }
+               10 => "X", 14 => "XIV", 19 => "XIX", 24 => "XXIV", 40 => "XL", 50 => "L", 
+               100 => "C", 400 => "CD", 500 => "D",
+               1000 => "M", 3000 => "MMM" } 
 
-describe "Work our way through ROMANTABLE" do
+
+describe "Test a bunch of edge cases." do
 
   ROMANTABLE.each do |arabic, roman| 
 
@@ -15,3 +18,11 @@ describe "Work our way through ROMANTABLE" do
 
 end
 
+describe "Handle overflow and underflow." do
+  it "should tell us when we've hit overflow" do
+    Arabic2Roman.convert(3001).should == "Overflow. Nothing larger than 3000."
+  end
+  it "should tell us when we've hit underflow" do
+    Arabic2Roman.convert(-1).should == "Underflow. Nothing less than zero."
+  end
+end
