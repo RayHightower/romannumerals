06ef0084efa6c28e29dea5b1e589e855bbb1e290
RayHightower <rth@wisdomgroup.com>
2012-04-16 21:02:19 -0500

    sometimes, it is possible to do too much refactoring at once time!

--- kata03/lib/arabic2roman.rb
+++ kata03/lib/arabic2roman.rb
@@ -6,46 +6,15 @@ class Arabic2Roman
 
     roman = ""
 
-    while arabic >= 90
-      roman += "XC"
-      arabic -= 90
+    ARABIC2ROMANTABLE.each do |arabictab, romantab|
+
+      while arabictab >= romantab
+        roman += romantab
+        arabic -= arabictab
+      end
+
     end
 
-    while arabic >= 50
-      roman += "L"
-      arabic -= 50
-    end
-
-    while arabic >= 40
-      roman += "XL"
-      arabic -= 40
-    end
-
-    while arabic >= 10
-      roman += "X"
-      arabic -= 10
-    end
-
-    while arabic >= 9
-      roman += "IX"
-      arabic -= 9
-    end
-
-    while arabic >= 5
-      roman += "V"
-      arabic -= 5
-    end
-
-    while arabic >= 4
-      roman += "IV"
-      arabic -= 4
-    end
-
-    while arabic >= 1 do
-      roman += "I"
-      arabic -= 1
-    end
-    
     return roman
 
   end
