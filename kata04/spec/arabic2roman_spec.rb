69c6e3371f009d00eadcea017b001bb52585e909
RayHightower <rth@wisdomgroup.com>
2012-04-20 12:56:02 -0500

    looping tests to save time & space

--- kata04/spec/arabic2roman_spec.rb
+++ kata04/spec/arabic2roman_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../lib/arabic2roman'
 
+ARABIC2ROMANTABLE = { 1 => "I", 3 => "III", 4 => "IV", 5 => "IV", 9 => "IX", 10 => "X", 30 => "XXX" }
+
 describe "do the first conversions" do
   it "should convert Arabic 1 to Roman I" do
     Arabic2Roman.convert(1).should == "I"
@@ -15,5 +17,14 @@ describe "do the first conversions" do
   end
   it "should convert Arabic 30 to Roman XXX" do
     Arabic2Roman.convert(30).should == "XXX"
+  end
+end
+
+describe "let's run the tests through a loop" do
+
+  ARABIC2ROMANTABLE.each do |arabtab, romantab|
+    it "should convert Arabic 30 to Roman XXX" do
+      Arabic2Roman.convert(arabtab).should == romantab
+    end
   end
 end
