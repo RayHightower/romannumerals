00a3cd0d33f96dc195d128249516e93600f3c104
RayHightower <rth@wisdomgroup.com>
2012-04-26 20:13:20 -0500

    I and II done

---
+++ kata05/lib/arabic2roman.rb
@@ -0,0 +1,6 @@
+class Arabic2Roman
+  def self.convert(arabic)
+    return "I" if arabic == 1
+    return "II" if arabic == 2
+  end
+end
--- kata05/spec/arabic2roman_spec.rb
+++ kata05/spec/arabic2roman_spec.rb
@@ -4,4 +4,7 @@ describe "convert Arabic to Roman" do
   it 'should convert 1 to I' do
     Arabic2Roman.convert(1).should == "I"
   end
+  it 'should convert 2 to II' do
+    Arabic2Roman.convert(2).should == "II"
+  end
 end
