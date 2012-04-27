1f3e30d388dc4780f6af4a65c288e6c95b2eddf6
RayHightower <rth@wisdomgroup.com>
2012-04-26 20:43:07 -0500

    okay... I get it :-)

--- kata05/lib/arabic2roman.rb
+++ kata05/lib/arabic2roman.rb
@@ -5,33 +5,13 @@ class Arabic2Roman
   def self.convert(arabic)
     roman = ""
 
-    CONVERTER.each do |arabicconv, romanconv|
+    CONVERTER.each do |arabickey, romanval|
 
-      while arabicconv >= arabic
-        roman += romanconv
-        arabic -= arabicconv
+      while arabic >= arabickey 
+        roman += romanval
+        arabic -= arabickey
       end
 
-    end
-
-    while arabic >= 10
-      roman += "X"
-      arabic -= 10
-    end
-
-    if arabic >= 5
-      roman += "V"
-      arabic -= 5
-    end
-
-    if arabic >= 4
-      roman += "IV"
-      arabic -= 4
-    end
-
-    while arabic >= 1
-      roman += "I"
-      arabic -= 1
     end
 
     return roman
