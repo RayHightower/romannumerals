3fd955c6e878c6b57aea25ef456e6c72f9aed2a7
RayHightower <rth@wisdomgroup.com>
2012-04-26 20:34:39 -0500

    iterations...

--- kata05/lib/arabic2roman.rb
+++ kata05/lib/arabic2roman.rb
@@ -1,6 +1,40 @@
 class Arabic2Roman
+
+  CONVERTER = { 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
+
   def self.convert(arabic)
-    return "I" if arabic == 1
-    return "II" if arabic == 2
+    roman = ""
+
+    CONVERTER.each do |arabicconv, romanconv|
+
+      while arabicconv >= arabic
+        roman += romanconv
+        arabic -= arabicconv
+      end
+
+    end
+
+    while arabic >= 10
+      roman += "X"
+      arabic -= 10
+    end
+
+    if arabic >= 5
+      roman += "V"
+      arabic -= 5
+    end
+
+    if arabic >= 4
+      roman += "IV"
+      arabic -= 4
+    end
+
+    while arabic >= 1
+      roman += "I"
+      arabic -= 1
+    end
+
+    return roman
+
   end
 end
--- kata05/spec/arabic2roman_spec.rb
+++ kata05/spec/arabic2roman_spec.rb
@@ -7,4 +7,19 @@ describe "convert Arabic to Roman" do
   it 'should convert 2 to II' do
     Arabic2Roman.convert(2).should == "II"
   end
+  it 'should convert 3 to III' do
+    Arabic2Roman.convert(3).should == "III"
+  end
+  it 'should convert 4 to IV' do
+    Arabic2Roman.convert(4).should == "IV"
+  end
+  it 'should convert 5 to V' do
+    Arabic2Roman.convert(5).should == "V"
+  end
+  it 'should convert 10 to X' do
+    Arabic2Roman.convert(10).should == "X"
+  end
+  it 'should convert 20 to XX' do
+    Arabic2Roman.convert(20).should == "XX"
+  end
 end
