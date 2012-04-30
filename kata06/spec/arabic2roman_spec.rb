b192562774c83b85f4b841c4a7dd02459063b09e
RayHightower <rth@wisdomgroup.com>
2012-04-29 22:10:34 -0500

    added a bunch of tests at once

--- kata06/spec/arabic2roman_spec.rb
+++ kata06/spec/arabic2roman_spec.rb
@@ -1,22 +1,18 @@
 require_relative '../lib/arabic2roman'
 
+CONVERTLIST = { 1 => 'I', 2 => 'II', 5 => 'V', 9 => 'IX', 10 => 'X', 
+                20 => 'XX', 24 => 'XXIV', 30 => 'XXX', 40 => 'XL', 50 => 'L' }
+
 describe 'given an Arabic number, return the corresponding Roman numeral' do
   it 'should return something' do
     Arabic2Roman.convert(1).should == 'I'
   end
-  it 'should return I, II, or III' do
-    Arabic2Roman.convert(2).should == 'II'
-  end
-  it 'should return V' do
-    Arabic2Roman.convert(5).should == 'V'
-  end
-  it 'should return IV' do
-    Arabic2Roman.convert(4).should == 'IV'
-  end
-  it 'should return X' do
-    Arabic2Roman.convert(10).should == 'X'
-  end
-  it 'should return IX' do
-    Arabic2Roman.convert(9).should == 'IX'
+end
+
+describe 'return the right Roman for each Arabic on the list' do
+  CONVERTLIST.each do |arabic, roman|
+    it 'should return the right conversion' do
+      Arabic2Roman.convert(arabic).should == roman
+    end
   end
 end
