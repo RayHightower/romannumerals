d0f34d30cb0a1be2242e2a47f46d38cab121a54e
RayHightower <rth@wisdomgroup.com>
2012-05-01 14:23:58 -0500

    added a few more tests

--- kata06/lib/arabic2roman.rb
+++ kata06/lib/arabic2roman.rb
@@ -1,6 +1,7 @@
 class Arabic2Roman
 
-  A2RCONV = { 100 => 'C', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
+  A2RCONV = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
+              90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
 
   def self.convert(arabic)
     roman = ''
--- kata06/spec/arabic2roman_spec.rb
+++ kata06/spec/arabic2roman_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../lib/arabic2roman'
 
 CONVERTLIST = { 1 => 'I', 2 => 'II', 5 => 'V', 9 => 'IX', 10 => 'X', 
-                20 => 'XX', 24 => 'XXIV', 30 => 'XXX', 40 => 'XL', 50 => 'L' }
+                20 => 'XX', 24 => 'XXIV', 30 => 'XXX', 40 => 'XL', 50 => 'L',
+                90 => 'XC', 100 => 'C', 500 => 'D', 900 => 'CM', 1000 => 'M',
+                1964 => 'MCMLXIV' }
 
 describe 'given an Arabic number, return the corresponding Roman numeral' do
   it 'should return something' do
