6817ac7290ae5aabfb9cec50e3ff10278a2589c4
RayHightower <rth@wisdomgroup.com>
2012-05-05 21:27:06 -0500

    kata complete

--- kata07/lib/arabic2roman.rb
+++ kata07/lib/arabic2roman.rb
@@ -1,6 +1,7 @@
 class Arabic2Roman
 
-  CONVERTERS = { 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
+  CONVERTERS = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL',
+                 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
   
   def self.calc(arabic)
 
--- kata07/spec/arabic2roman_spec.rb
+++ kata07/spec/arabic2roman_spec.rb
@@ -1,6 +1,8 @@
 require_relative '../lib/arabic2roman'
 
-ARABICCONV = { 1 => 'I', 2 => 'II', 4 => 'IV', 5 => 'V' }
+ARABICCONV = { 1 => 'I', 2 => 'II', 4 => 'IV', 5 => 'V', 9 => 'IX', 20 => 'XX', 40 => 'XL', 50 => 'L', 90 => 'XC',
+               100 => 'C', 400 => 'CD', 500 => 'D', 900 => 'CM', 1000 => 'M', 3000 => 'MMM' }
+
 
 describe 'go thru ARABICCONV and test each' do
   ARABICCONV.each do |arabic, roman| 
