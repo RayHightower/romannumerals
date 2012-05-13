36f63888f26ea5a39cfd027f428905b0046c11cf
RayHightower <rth@wisdomgroup.com>
2012-05-13 10:17:39 -0500

    must use the old hashrocket syntax if the keys are integers

--- kata07/spec/arabic2roman_spec.rb
+++ kata07/spec/arabic2roman_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/arabic2roman'
 
-ARABICCONV = { 1 => 'I', 2 => 'II', 4 => 'IV', 5 => 'V', 9 => 'IX', 20 => 'XX', 40 => 'XL', 50 => 'L', 90 => 'XC',
+  # Must use the old hashrocket syntax if integers are used as keys.
+  ARABICCONV = { 1 => "I", 2 => "II", 4 => "IV", 5 => 'V', 9 => 'IX', 20 => 'XX', 40 => 'XL', 50 => 'L', 90 => 'XC',
                100 => 'C', 400 => 'CD', 500 => 'D', 900 => 'CM', 1000 => 'M', 3000 => 'MMM' }
 
 
