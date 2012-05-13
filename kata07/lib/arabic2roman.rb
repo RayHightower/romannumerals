8d2ef98bc32754f70e746dcd3b4c225763bb419d
RayHightower <rth@wisdomgroup.com>
2012-05-13 00:52:23 -0500

    exploring inheritance

--- kata07/lib/arabic2roman.rb
+++ kata07/lib/arabic2roman.rb
@@ -3,6 +3,16 @@ class Arabic2Roman
   CONVERTERS = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL',
                  10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
   
+  def self.zclassinfo
+    j = CONVERTERS.class
+    return j
+  end
+
+  def self.zhello(valuething)
+    puts 'We are in the hello method.'
+    return valuething
+  end
+
   def self.calc(arabic)
 
     roman = ''
