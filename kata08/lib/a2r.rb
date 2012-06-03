586d0eff5d62b9cdeed325f66daf59c3ef89f629
RayHightower <rth@wisdomgroup.com>
2012-06-03 00:05:28 -0500

    first passing test

--- kata08/lib/a2r.rb
+++ kata08/lib/a2r.rb
@@ -1,2 +1,10 @@
 # Let's do it again!
 #
+#
+class A2r
+
+  def self.calc(arabic)
+    return 'I'
+    
+  end
+end
--- kata08/spec/a2r_spec.rb
+++ kata08/spec/a2r_spec.rb
@@ -1,3 +1,7 @@
 require_relative '../lib/a2r'
 
-
+describe 'write the first test' do
+  it 'should return I for 1' do
+    A2r.calc(1).should == 'I'
+  end
+end
