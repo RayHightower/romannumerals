330e9699b49485014497982c62b6a644384533a5
RayHightower <rth@wisdomgroup.com>
2012-06-06 23:57:21 -0500

    exercise is always good

--- kata08/lib/a2r.rb
+++ kata08/lib/a2r.rb
@@ -1,7 +1,8 @@
 class A2r
 
   def self.calc(arabic)
-    return 'I'
+    return 'I' if arabic == 1
+    return 'II' if arabic ==2
     
   end
 
--- kata08/spec/a2r_spec.rb
+++ kata08/spec/a2r_spec.rb
@@ -4,4 +4,7 @@ describe 'write the first test' do
   it 'should return I for 1' do
     A2r.calc(1).should == 'I'
   end
+  it 'should return II for 2' do
+    A2r.calc(2).should == 'II'
+  end
 end
