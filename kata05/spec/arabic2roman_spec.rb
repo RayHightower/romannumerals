218f1e9d022f314030da9ff76a39823715b6a6d1
Raymond T. Hightower <rth@wisdomgroup.com>
2015-01-18 19:23:28 -0600

    convert kata05 to expect syntax

--- kata05/spec/arabic2roman_spec.rb
+++ kata05/spec/arabic2roman_spec.rb
@@ -2,24 +2,24 @@ require_relative '../lib/arabic2roman'
 
 describe "convert Arabic to Roman" do
   it 'should convert 1 to I' do
-    Arabic2Roman.convert(1).should == "I"
+    expect(Arabic2Roman.convert(1)).to.eq "I"
   end
   it 'should convert 2 to II' do
-    Arabic2Roman.convert(2).should == "II"
+    expect(Arabic2Roman.convert(2)).to.eq "II"
   end
   it 'should convert 3 to III' do
-    Arabic2Roman.convert(3).should == "III"
+    expect(Arabic2Roman.convert(3)).to.eq "III"
   end
   it 'should convert 4 to IV' do
-    Arabic2Roman.convert(4).should == "IV"
+    expect(Arabic2Roman.convert(4)).to.eq "IV"
   end
   it 'should convert 5 to V' do
-    Arabic2Roman.convert(5).should == "V"
+    expect(Arabic2Roman.convert(5)).to.eq "V"
   end
   it 'should convert 10 to X' do
-    Arabic2Roman.convert(10).should == "X"
+    expect(Arabic2Roman.convert(10)).to.eq "X"
   end
   it 'should convert 20 to XX' do
-    Arabic2Roman.convert(20).should == "XX"
+    expect(Arabic2Roman.convert(20)).to.eq "XX"
   end
 end
