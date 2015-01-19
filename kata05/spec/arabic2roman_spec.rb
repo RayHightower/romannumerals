cc3d695357c09ea14a9da57edd996765d2408c3c
Raymond T. Hightower <rth@wisdomgroup.com>
2015-01-18 19:25:03 -0600

    typo correction

--- kata05/spec/arabic2roman_spec.rb
+++ kata05/spec/arabic2roman_spec.rb
@@ -2,24 +2,24 @@ require_relative '../lib/arabic2roman'
 
 describe "convert Arabic to Roman" do
   it 'should convert 1 to I' do
-    expect(Arabic2Roman.convert(1)).to.eq "I"
+    expect(Arabic2Roman.convert(1)).to eq "I"
   end
   it 'should convert 2 to II' do
-    expect(Arabic2Roman.convert(2)).to.eq "II"
+    expect(Arabic2Roman.convert(2)).to eq "II"
   end
   it 'should convert 3 to III' do
-    expect(Arabic2Roman.convert(3)).to.eq "III"
+    expect(Arabic2Roman.convert(3)).to eq "III"
   end
   it 'should convert 4 to IV' do
-    expect(Arabic2Roman.convert(4)).to.eq "IV"
+    expect(Arabic2Roman.convert(4)).to eq "IV"
   end
   it 'should convert 5 to V' do
-    expect(Arabic2Roman.convert(5)).to.eq "V"
+    expect(Arabic2Roman.convert(5)).to eq "V"
   end
   it 'should convert 10 to X' do
-    expect(Arabic2Roman.convert(10)).to.eq "X"
+    expect(Arabic2Roman.convert(10)).to eq "X"
   end
   it 'should convert 20 to XX' do
-    expect(Arabic2Roman.convert(20)).to.eq "XX"
+    expect(Arabic2Roman.convert(20)).to eq "XX"
   end
 end
